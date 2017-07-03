0109a83f5db5a0b15501085ec188e1945e19bdbc
Kai De Melo <kaidemelo7@gmail.com>
2017-07-03 16:37:52 +0100

    C# stuff

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    class Book
+    {
+        string title;
+
+        public void setTitle(string newTitle)
+        {
+            title = newTitle;
+        }
+
+        public string getTitle()
+        {
+            return title;
+        }
+    }
+}
---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! ");
+            var firstBook = new Book();
+            firstBook.setTitle("Hunger Games");
+            var secondBook = new Book();
+            secondBook.setTitle("Cooking Book");
+            var library = new List<Book>();
+            library.Add(firstBook);
+            library.Add(secondBook);
+
+            try
+            {
+                var bookIndex = int.Parse(Console.ReadLine());
+                Console.WriteLine(library[bookIndex].getTitle());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Na fam.");
+            }
+        }
+    }
+}
